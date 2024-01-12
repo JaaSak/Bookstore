6630229454e7a14dd01ffe50f9a4712c9e853508
Jaana Saksi <jaana.saksi@desenhor.fi>
2024-01-12 14:56:11 +0200

    setup gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/bookstorereact/', // add this property and the value is the name of your repository
+  base: '/bookstorereact/', 
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  } 
 })
+
