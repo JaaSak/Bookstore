aa746f8bd89ce961f61eb8f458e0de873cd43741
Jaana Saksi <jaana.saksi@desenhor.fi>
2024-01-12 15:13:04 +0200

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    chunkSizeWarningLimit: 100000000
+},
   base: '/bookstorereact/', 
   plugins: [react()],
   test: {
     globals: true,
     environment: 'jsdom',
   } 
+  
 })
 
